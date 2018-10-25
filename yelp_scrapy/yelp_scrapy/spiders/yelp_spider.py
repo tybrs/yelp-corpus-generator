e7f02f5f475e806b86708aaa7c4e48ee084f2d40
Tyler Wilbers <gavagai@archy.localdomain>
2018-10-25 11:45:50 -0400

    changed amount of total requests

--- yelp_scrapy/yelp_scrapy/spiders/yelp_spider.py
+++ yelp_scrapy/yelp_scrapy/spiders/yelp_spider.py
@@ -18,8 +18,8 @@ class YelpSpider(Spider):
         # '//span[@class="pagination-results-window"]/text()').extract_first()
         # number_of_pages = int(findall('\s+(\d+)\s+', number_of_pages)[0])
         nyc_urls = [
-            'https://www.yelp.com/search?find_desc'
-            '=Restaurants&find_loc=New%20York%2C%20NY&'
+            'https://www.yelp.com/search?find_desc='
+            'Restaurants&find_loc=New%20York%2C%20NY&'
             'sortby=review_count&start={}'.format(x)
             for x in range(0, 960 + 1, 30)]
 
@@ -47,23 +47,23 @@ class YelpSpider(Spider):
             'sortby=review_count&start={}'.format(x)
             for x in range(0, 960 + 1, 30)]
 
-        for url in (nyc_urls[:1] + chi_urls[:1] + 
-            la_urls[:1] + orlando_urls[:1] + lv_urls[:1]):
-            sleep(randint(1,2))
+        for url in (nyc_urls + chi_urls + 
+            la_urls + orlando_urls + lv_urls):
+            sleep(randint(1, 2))
             yield Request(url=url, callback=self.parse_search)
 
     def parse_search(self, response):
-        print("=" * 50)
+        print("=search=" * 30)
         bizurls = response.xpath(
             '//a[@class="biz-name js-analytics-click"]/@href').extract()
         bizurls = ['https://yelp.com' + url for url in bizurls][1:]
         print(bizurls)
         for url in bizurls:
-            sleep(randint(1,2))
+            sleep(randint(1, 2))
             yield Request(url=url, callback=self.parse_biz_page)
 
     def parse_biz_page(self, response):
-        print("%" * 50)
+        print(r"%biz%" * 30)
         num_reviews_str = response.xpath(
             '//div[@class="page-of-pages '
             'arrange_unit arrange_unit--fill"]/text()').extract_first()
@@ -71,7 +71,8 @@ class YelpSpider(Spider):
         if num_reviews_str:
             num_reviews = int(findall('of (\d+)', num_reviews_str)[0])
             review_portion = int(20 * round(num_reviews * 2 / 20))
-            reviews_range = [1] + list(range(20, num_reviews * 20, review_portion))
+            reviews_range = [1] + list(
+                range(20, num_reviews * 20, review_portion))
         else:
             reviews_range = [1]
 
@@ -102,7 +103,7 @@ class YelpSpider(Spider):
         # item['business_link'] = business_link
 
         for url in review_urls[:1]:
-            sleep(randint(1,2))
+            sleep(randint(1, 2))
             yield Request(url=url,
                           meta={'business_name': business_name,
                                 'business_city': business_city,
@@ -113,7 +114,7 @@ class YelpSpider(Spider):
                           callback=self.parse_reviews)
 
     def parse_reviews(self, response):
-        print("*" * 50)
+        print("*reviews*" * 30)
         business_name = response.meta['business_name']
         business_city = response.meta['business_city']
         business_zip = response.meta['business_zip']
