0429addc8500c7f2f407a4649c00a764a18f4d4f
Tyler Wilbers <gavagai@archy.localdomain>
2019-09-29 22:32:17 -0400

    removed collections import

--- src/yelp_scrapy/spiders/yelp_spider.py
+++ src/yelp_scrapy/spiders/yelp_spider.py
@@ -3,7 +3,6 @@ from scrapy_splash import SplashRequest
 from yelp_scrapy.items import UserItem, BizItem
 from re import findall
 from yaml import load, SafeLoader
-from collections import defaultdict
 from scrape_utils import print_progress, get_urls
 # from time import sleep
 
